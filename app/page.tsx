715bb83892c36977df62fe798054d952fca0cfae
Jordi <jordi.cahuas20@gmail.com>
2025-01-20 09:20:53 -0500

    add show image

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,14 @@ import React, { useState, ChangeEvent, FormEvent } from 'react';
 
 function App() {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
   const [apiResponse, setApiResponse] = useState<string>('No response yet...');
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>): void => {
     if (event.target.files) {
-      setSelectedFile(event.target.files[0]);
+      const file = event.target.files[0];
+      setSelectedFile(file);
+      setPreviewUrl(URL.createObjectURL(file)); // Genera la URL de vista previa
     }
   };
 
@@ -45,25 +48,40 @@ function App() {
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
-      <form 
-        onSubmit={handleSubmit} 
-        className="bg-white shadow-md rounded-lg p-6 w-full max-w-md space-y-4">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded-lg p-6 w-full max-w-md space-y-4"
+      >
         <h1 className="text-2xl font-bold text-gray-700 text-center">Number Prediction</h1>
-        <label htmlFor="fileInput" className="block text-gray-600 font-medium">Upload an image:</label>
-        <input 
-          type="file" 
-          id="fileInput" 
-          accept="image/*" 
-          onChange={handleFileChange} 
-          required 
+        <label htmlFor="fileInput" className="block text-gray-600 font-medium">
+          Upload an image:
+        </label>
+        <input
+          type="file"
+          id="fileInput"
+          accept="image/*"
+          onChange={handleFileChange}
+          required
           className="block w-full px-4 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-        <button 
-          type="submit" 
-          className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-200">
+        <button
+          type="submit"
+          className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-200"
+        >
           Predict
         </button>
       </form>
+
+      {previewUrl && (
+        <div className="mt-4 w-md max-w-md">
+          <h2 className="text-lg font-bold text-gray-700 text-center">Image Preview</h2>
+          <img
+            src={previewUrl}
+            alt="Selected preview"
+            className="mt-2 w-full rounded-lg shadow-md"
+          />
+        </div>
+      )}
 
       <div className="mt-8 w-full max-w-md bg-white shadow-md rounded-lg p-6">
         <h2 className="text-xl font-bold text-gray-700 text-center">API Response</h2>
